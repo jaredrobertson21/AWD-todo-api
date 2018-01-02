b7da0eace39d2a7aaa01fb05d04613b5f66c20a9
Jared Robertson <jared.robertson@usask.ca>
2018-01-02 08:49:22 -0600

    Todos can be added to web page

--- public/app.js
+++ public/app.js
@@ -4,15 +4,39 @@ $(document).ready(function() {
     .catch(function(err) {
         console.log(err)
     });
+
+    $('#todoInput').keypress(function(event) {
+        // event listener for enter key
+        if (event.which == 13) {
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos) {
     // add todos to page
     todos.forEach(function(todo) {
-        var newTodo = $(`<li class="task">${todo.name}</li>`);
-        if (todo.completed) {
-            newTodo.addClass("done");
-        }
-        $('.list').append(newTodo);
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo) {
+    var newTodo = $(`<li class="task">${todo.name}</li>`);
+    if (todo.completed) {
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo(){
+    // send request to create new todo
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo) {
+        addTodo(newTodo);
+        $('#todoInput').val('');
+    })
+    .catch(function(err) {
+        console.log(err);
     });
 }
